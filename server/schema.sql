6fff66295f7f1e6586c8f41aedd6cdcdd071caca
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-21 21:20:14 -0700

    Create database

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,73 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+-- ---
+-- Globals
+-- ---
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+
+-- -
+
+-- DROP TABLE IF EXISTS messages;
+		
+CREATE TABLE users (
+  userid INT AUTO_INCREMENT NOT NULL,
+  username INT NULL,
+  PRIMARY KEY (userid)
+);
+		
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT NOT NULL,
+  messageid INT NULL,
+  userid INT NULL,
+  roomid INT NULL,
+  message INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userid) REFERENCES users (userid)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+-- DROP TABLE IF EXISTS users;
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+-- ALTER TABLE messages ADD FOREIGN KEY (userid) REFERENCES users (userid);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`messageid`,`userid`,`roomid`,`message`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`userid`,`username`) VALUES
+-- ('','');
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+ *  to create the database and the tables.*/ 
 
